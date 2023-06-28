f4e7ca80dab64cfbaa18492d20b92f912d59b63a
sjakes201 <sjakes201@gmail.com>
2023-06-28 00:18:07 -0400

    first api test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import logo from './homie.png';
 import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+
+  const [response, setResponse] = useState("")
+  const [num, setNum] = useState(0)
+
+  let go = async () => {
+    let ressy = await fetch('https://farm-api.azurewebsites.net/api/greetme', {
+      method: 'GET', // or 'POST', 'PUT', etc. depending on your function
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      // If your function needs a request body, include it here
+    }).then((x) => x.text());
+    setResponse(ressy)
+    setNum( (oldNum) => oldNum + 1);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +27,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>{response}</p>
+        <p>calls: {num}</p>
+        <button onClick={go}>API CALL</button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,7 +39,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div >
   );
 }
 
