94033a9bc16f71d91eb8627a20701f65e7445d96
sjakes201 <sjakes201@gmail.com>
2023-08-23 11:49:26 -0400

    buffed machines

--- src/MACHINESINFO.js
+++ src/MACHINESINFO.js
@@ -13,7 +13,7 @@ module.exports = {
     cheeseMachineInfo: {
         validInputs: ['cow_milk', 'yak_milk', 'goat_milk'],
         tier1: {
-            capacity: 5,
+            capacity: 10,
             timeInMs: 1200000, //20 mins (4 min each)
             /* probabilities is probability of q3, then q2, then q1, then q0. The first one the random() is less than is quality
             ex: [0.10, 0.40, 0.60, 1.00] means 10% chance of q3, 30% chance of q2, 20% of q1, 40% of q0, based on probability of being NOT below earlier but earlier than current elem
@@ -22,20 +22,20 @@ module.exports = {
             probabilities: [0.00, 0.10, 0.40, 1.00]
         },
         tier2: {
-            capacity: 10,
+            capacity: 25,
             timeInMs: 2100000, //35 mins (3.5 min each)
             probabilities: [0.10, 0.50, 0.90, 1.00]
         },
         tier3: {
-            capacity: 25,
+            capacity: 40,
             timeInMs: 3000000, //50 mins (2 min each)
-            probabilities: [0.25, 0.70, 1.00, -1]
+            probabilities: [0.25, 0.70, 1.00, -1.00]
         },
     },
     clothMachineInfo: {
         validInputs: ['sheep_wool', 'llama_wool'],
         tier1: {
-            capacity: 5,
+            capacity: 10,
             timeInMs: 1200000, //20 mins (4 min each)
             /* probabilities is probability of q3, then q2, then q1, then q0. The first one the random() is less than is quality
             ex: [0.10, 0.40, 0.60, 1.00] means 10% chance of q3, 30% chance of q2, 20% of q1, 40% of q0, based on probability of being NOT below earlier but earlier than current elem
@@ -44,20 +44,20 @@ module.exports = {
             probabilities: [0.00, 0.10, 0.40, 1.00]
         },
         tier2: {
-            capacity: 10,
+            capacity: 25,
             timeInMs: 2100000, //35 mins (3.5 min each)
             probabilities: [0.10, 0.50, 0.90, 1.00]
         },
         tier3: {
-            capacity: 25,
+            capacity: 40,
             timeInMs: 3000000, //50 mins (2 min each)
-            probabilities: [0.25, 0.70, 1.00, -1]
+            probabilities: [0.25, 0.70, 1.00, -1.00]
         },
     },
     mayonnaiseMachineInfo: {
         validInputs: ['chicken_egg', 'duck_egg', 'quail_egg', 'ostrich_egg', 'kiwi_egg'],
         tier1: {
-            capacity: 5,
+            capacity: 10,
             timeInMs: 1200000, //20 mins (4 min each)
             /* probabilities is probability of q3, then q2, then q1, then q0. The first one the random() is less than is quality
             ex: [0.10, 0.40, 0.60, 1.00] means 10% chance of q3, 30% chance of q2, 20% of q1, 40% of q0, based on probability of being NOT below earlier but earlier than current elem
@@ -66,12 +66,12 @@ module.exports = {
             probabilities: [0.00, 0.10, 0.40, 1.00]
         },
         tier2: {
-            capacity: 10,
+            capacity: 25,
             timeInMs: 2100000, //35 mins (3.5 min each)
             probabilities: [0.10, 0.50, 0.90, 1.00]
         },
         tier3: {
-            capacity: 25,
+            capacity: 40,
             timeInMs: 3000000, //50 mins (2 min each)
             probabilities: [0.25, 0.70, 1.00, -1.00]
         },
